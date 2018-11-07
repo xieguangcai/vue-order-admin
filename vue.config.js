82e799dbf7db9ee76c21cef9b9f33a452a5793ca
xieguangcai <xieguangcai@coocaa.com>
2018-11-07 22:29:22 +0800

    1. 修改了打包的文件名称配置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
+const uglify = require('uglifyjs-webpack-plugin');
+
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/' : '/',
   pwa: {
     name: 'vue-order-admin'
+  },
+  configureWebpack:{
+    plugins:[
+      new uglify()
+    ],
+    output:{
+      filename: '[name].[hash:5].js'
+    }
   }
 }
 // https://blog.csdn.net/sunny_desmond/article/details/80916706 一篇说明文章
